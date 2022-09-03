ef31a2e695e86401f7eb489c0d9fe94d19f89b93
Hamza Elkotb <61008779+HamzaElkotp@users.noreply.github.com>
2022-09-03 16:45:55 +0300

    make HTML CSS, JS Basics
    recorded Problems:
    1- console not working
    2- object tree not appear

---
+++ main.js
@@ -0,0 +1,42 @@
+let outScreen = window.OutScreen;
+let codeInp = document.getElementById("CMinput");
+
+function newInput(code){
+    let inputT = document.createElement("p");
+    inputT.classList.add("code","inputT");
+    inputT.textContent = `> ${code}`;
+    outScreen.append(inputT);
+}
+
+function newOutput(code){
+    let outPutT = document.createElement("p");
+    outPutT.classList.add("code","outputT")
+    try{
+        outPutT.textContent = eval(code); 
+    }
+        
+    catch(err){
+        outPutT.textContent = `${code, err}`;
+    }
+    outScreen.append(outPutT);
+}
+
+function factory(){
+    let code = document.getElementById("CMinput").value;
+    newInput(code);
+    newOutput(code);
+}
+
+codeInp.addEventListener("keydown",function(e){
+    if (e.key === "Enter" && !e.shiftKey){
+        factory()
+        codeInp.value = ""
+    }
+    
+})
+
+
+
+
+
+
