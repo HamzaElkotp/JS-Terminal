1064430e817fd7f2214b0befc10c76b3fe64f5a4
Hamza-business <hamzadev1business@gmail.com>
2023-01-01 16:16:36 +0300

    remake funcdmetails and discover bugs to solve

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Fundmetals of Terminal script
 let outScreen = window.OutScreen;
 let codeInp = document.getElementById("CMinput");
 
@@ -40,6 +41,9 @@ codeInp.addEventListener("keydown",function(e){
 })
 
 
+
+
+// Code of Terminal Settings and controle
 class Style{
     constructor(TbgC,IbgC,InC,OuC,ErC){
         this.TbgC = TbgC;
@@ -53,7 +57,7 @@ class Style{
 
 
 let root = document.querySelector(':root');
-let boxes = Array.from(document.querySelectorAll('.Aboxes .boxes'));
+let boxes = [...document.querySelectorAll('.Aboxes .boxes')];
 let Aboxes = document.querySelector('.Aboxes');
 
 function applyStyle(inx){
@@ -63,18 +67,6 @@ function applyStyle(inx){
     root.style.setProperty('--output', JSON.parse(localStorage.styles)[inx].OuC);
     root.style.setProperty('--err', JSON.parse(localStorage.styles)[inx].ErC);
 }
-
-
-window.addEventListener('load',function(){
-    if(localStorage.style == undefined){
-        let styles = [new Style("#111","#1d1d1d","#fff","#00ffcc","#ff0040")];
-        localStorage.setItem("styles", JSON.stringify(styles));
-        localStorage.style = 0;
-        applyStyle(0);
-    }else{
-        applyStyle(localStorage.style);
-    }
-})
 
 
 let cp = document.querySelector('.colorsPanle');
@@ -89,77 +81,124 @@ function valgetter(){
     )
 }
 
-function applyFun(){
-    let vals = valgetter()
-    root.style.setProperty('--bgColor',vals.TbgC);
-    root.style.setProperty('--inpArea',vals.IbgC);
-    root.style.setProperty('--input',vals.InC);
-    root.style.setProperty('--output',vals.OuC);
-    root.style.setProperty('--ere',vals.ErC);
-}
+// function applyFun(){
+//     let vals = valgetter()
+//     root.style.setProperty('--bgColor',vals.TbgC);
+//     root.style.setProperty('--inpArea',vals.IbgC);
+//     root.style.setProperty('--input',vals.InC);
+//     root.style.setProperty('--output',vals.OuC);
+//     root.style.setProperty('--ere',vals.ErC);
+// }
 
-function saveFun(e){
-    let vals = valgetter();
-    let newObj = new Style(vals.TbgC,vals.IbgC,vals.InC,vals.OuC,vals.ErC);
-    let arr = JSON.parse(localStorage.styles);
-    let index = e.textContent - 1;
-    arr[index]=newObj;
-    localStorage.setItem('styles',JSON.stringify(arr));
-    localStorage.style = index;
-    applyFun()
-}
+// function saveFun(e){
+//     let vals = valgetter();
+//     let newObj = new Style(vals.TbgC,vals.IbgC,vals.InC,vals.OuC,vals.ErC);
+//     let arr = JSON.parse(localStorage.styles);
+//     let index = e.textContent - 1;
+//     arr[index]=newObj;
+//     localStorage.setItem('styles',JSON.stringify(arr));
+//     localStorage.style = index;
+//     applyFun()
+// }
 
-function downFun(){
-    let vals = valgetter();
-    let data = JSON.stringify(vals);
-    let file = new Blob([data],{type:"txt"});
-    let a = document.createElement("a"), url = URL.createObjectURL(file);
-    a.href = url;
-    a.classList.add("hide")
-    a.download = file;
-    document.body.appendChild(a);
-    a.click()
-}
+// function downFun(){
+//     let vals = valgetter();
+//     let data = JSON.stringify(vals);
+//     let file = new Blob([data],{type:"txt"});
+//     let a = document.createElement("a"), url = URL.createObjectURL(file);
+//     a.href = url;
+//     a.classList.add("hide")
+//     a.download = file;
+//     document.body.appendChild(a);
+//     a.click()
+// }
 
-function delFun(e){
-    let index = e.textContent - 1;
-    let arr = JSON.parse(localStorage.styles);
-    arr.splice(index,1)
-    boxes.splice(index,1)
-    localStorage.setItem('styles',JSON.stringify(arr));
-    localStorage.style = 0;
-    e.remove();
-    cp.classList.add('hide');
-}
+// function delFun(e){
+//     let arr = JSON.parse(localStorage.styles);
+//     arr.splice(e,1)
+//     boxes.splice(e,1)
+//     localStorage.setItem('styles',JSON.stringify(arr));
+//     localStorage.style = 0;
+//     boxes[e].remove();
+//     cp.classList.remove('hide');
+// }
 
-function cpevent(){
-    cp.classList.toggle('hide');
-    let apply = document.getElementById("apply");
-    apply.addEventListener('click',applyFun);
-    let save = document.getElementById("save");
-    save.addEventListener('click',()=>{return saveFun(this)});
-    let down = document.getElementById("down");
-    down.addEventListener('click',downFun);
-    let del = document.getElementById("del");
-    del.addEventListener('click',()=>{
-        return delFun(this)
-    });
-}
+// function cpevent(index){
+//     cp.classList.toggle('hide');
+//     let apply = document.getElementById("apply");
+//     apply.addEventListener('click',applyFun);
+
+//     let save = document.getElementById("save");
+//     save.addEventListener('click',()=>{
+//         return saveFun(index)
+//     });
+
+//     let down = document.getElementById("down");
+//     down.addEventListener('click',downFun);
+
+//     let del = document.getElementById("del");
+//     del.addEventListener('click',()=>{
+//         return delFun(index)
+//     });
+// }
 
 
 
 let addBox = document.querySelector('.add.boxes');
 addBox.addEventListener('click', function(){
     if(boxes.length < 3){
+        boxes = [...document.querySelectorAll('.Aboxes .boxes')];
         let newBox = `<div class="boxes">${boxes.length + 1}</div>`;
         Aboxes.innerHTML += newBox;
-        boxes = Array.from(document.querySelectorAll('.Aboxes .boxes'));
-        boxes[boxes.length - 1].addEventListener('click',cpevent);
+
+        // addTable();
         boxes[boxes.length - 1].click();
         let stylist = JSON.parse(localStorage.styles);
         stylist.push("");
         localStorage.styles = JSON.stringify(stylist);
     }
+    if(boxes.length >= 2){
+        addBox.remove();
+    }
+});
+
+
+function addTable(){
+    boxes = [...document.querySelectorAll('.Aboxes .boxes')];
+    // boxes.forEach((elo,i)=>{
+    //     elo.removeEventListener('click',function(){cpevent(i)})
+    //     elo.addEventListener('click',function(){
+    //         cpevent(i)
+    //     })
+    // })
+}
+
+function localStyleDom(){
+    let styles = JSON.parse(localStorage.styles);
+    for(let i=0;i<styles.length;i++){
+        let newBox = `<div class="boxes">${i+1}</div>`;
+        Aboxes.innerHTML += newBox;
+        // addTable();
+    }
+}
+
+
+window.addEventListener('load',function(){
+    // Defualt Style
+    if(localStorage.style == undefined){
+        let styles = [new Style("#111","#1d1d1d","#fff","#00ffcc","#ff0040")];
+        localStorage.setItem("styles", JSON.stringify(styles));
+        localStorage.style = 0;
+        applyStyle(0);
+    }else{
+        applyStyle(localStorage.style);
+    }
+
+    // Add styles to DOM as buttons
+    localStyleDom();
+
+    // Check if there is more than 2 boxes to remove add button
+    boxes = [...document.querySelectorAll('.Aboxes .boxes')]
     if(boxes.length == 3){
         addBox.remove();
     }
